35bd62e1596a5d1832f3dd9a3bb74b329ceea6a4
Kovalev Ivan <kovalevvanc@gmail.com>
2025-08-22 16:35:01 +0300

    Update: better UI in 2nd minigame

--- Assets/Scripts/SecondMinigame/UIManager.cs
+++ Assets/Scripts/SecondMinigame/UIManager.cs
@@ -1,6 +1,8 @@
 using System;
 using TMPro;
 using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
 
 namespace SecondMinigame
 {
@@ -17,7 +19,32 @@ namespace SecondMinigame
 
         public void ChangeCurrentTargetBuilding(string nextTargetName)
         {
+            StartCoroutine(ChangeTargetBuildingText(nextTargetName));
+        }
+
+        private IEnumerator ChangeTargetBuildingText(string nextTargetName)
+        {
+            float elapsedTime = 0f;
+            while (elapsedTime < 1f)
+            {
+                float newAlpha = Mathf.Lerp(1f, 0f, elapsedTime / 1f);
+                targetText.alpha = newAlpha;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            targetText.alpha = 0f;
             targetText.text = nextTargetName;
+            elapsedTime = 0f;
+            while (elapsedTime < 1f)
+            {
+                float newAlpha = Mathf.Lerp(0f, 1f, elapsedTime / 1f);
+                targetText.alpha = newAlpha;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            targetText.alpha = 1f;
         }
     }
 }
