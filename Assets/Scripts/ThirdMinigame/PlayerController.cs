0eaf73fc2daa2cafa1e08081d948001c8a146f53
Kovalev Ivan <kovalevvanc@gmail.com>
2025-08-29 13:25:14 +0300

    Add: coin pick up audio in 3rd minigame; tiles fall in 3rd minigame

--- Assets/Scripts/ThirdMinigame/PlayerController.cs
+++ Assets/Scripts/ThirdMinigame/PlayerController.cs
@@ -1,19 +1,21 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 namespace ThirdMinigame
 {
     public class PlayerController : MonoBehaviour
     {
         private GameObject _currentTile;
-
         private int _coinsCollected;
+        [SerializeField] private AudioSource coinPickUp;
         
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Coin"))
             {
                 _coinsCollected++;
+                coinPickUp.Play();
                 Destroy(other.gameObject);
             }
 
@@ -21,7 +23,7 @@ namespace ThirdMinigame
             {
                 if (_currentTile != null)
                 {
-                    Destroy(_currentTile);
+                    StartCoroutine(TileFall(_currentTile));
                 }
                 _currentTile = other.gameObject;
             }
@@ -31,6 +33,20 @@ namespace ThirdMinigame
                 GameManager.Instance.CompleteLevel(_coinsCollected);
                 _coinsCollected = 0;
             }
+        }
+
+        private IEnumerator TileFall(GameObject tile)
+        {
+            float elapsedTime = 0;
+            while (elapsedTime < 2)
+            {
+                float newY = Mathf.Lerp(0.25f, -5, elapsedTime / 2);
+                tile.transform.position = new Vector3(tile.transform.position.x, newY, tile.transform.position.z);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            tile.transform.position = new Vector3(tile.transform.position.x, -5, tile.transform.position.z);
+            Destroy(tile);
         }
     }
 }
